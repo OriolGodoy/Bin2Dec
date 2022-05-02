c035359dd55e32ffdf0001e82f0d09607bfdcbd4
oriol <10007468joan.23@fje.edu>
2022-05-02 13:44:10 +0200

    sba

--- js/main2.js
+++ js/main2.js
@@ -1,17 +1,16 @@
 function HexaJS() {
     var texto = document.getElementById("texto-1")
-    var input = document.getElementById("numero").value 
+    var input = document.getElementById("numero").value
     var error = document.getElementById("error")
     var final = document.getElementById("resultado")
     final.innerHTML = parseInt(input, 16)
     var errores = input.charAt(input.length - 1)
-    if (errores == 0 || errores == 1 || errores == 2 || errores == 3 || errores == 4 || errores == 5 || errores == 6 || errores == 7 || errores == 8 || errores == 9 || errores == "A" || errores == "B" || errores == "C" || errores == "D" || errores == "E" || errores == "F" || errores == "a" || errores == "b" || errores == "c" || errores == "d" || errores == "e" || errores == "f" ) {
+    if (errores == 0 || errores == 1 || errores == 2 || errores == 3 || errores == 4 || errores == 5 || errores == 6 || errores == 7 || errores == 8 || errores == 9 || errores == "A" || errores == "B" || errores == "C" || errores == "D" || errores == "E" || errores == "F" || errores == "a" || errores == "b" || errores == "c" || errores == "d" || errores == "e" || errores == "f") {
         texto.innerHTML = "Número convertido"
         error.innerHTML = ''
-    }
-    else {
+    } else {
         texto.innerHTML = ""
-        error.innerHTML = "You entered a non-hexadecimal digit (please enter only 0 to 9 and A to F)"
+        error.innerHTML = "Entra un digito Hexadecimal"
     }
     if (numero == '') {
         texto.innerHTML = ''
--- js/main3.js
+++ js/main3.js
@@ -1,12 +1,40 @@
 function ChangeJS() {
     var hexadecimal = document.getElementById("hexa")
-    var input = document.getElementById("select").value 
+    var input = document.getElementById("select").value
     var binario = document.getElementById("bin")
-    if (input = document.getElementById("bin") {
-        input.innerHTML = function OriJS()
-    }
+    var aplicacion = {
 
-    if (input = document.getElementById("hexa"){
-        input.innerHTML = function HexaJS()
+        campoTexto: null,
+        lista: null,
+
+        funcion1: function OriJS() {
+            // lo que querés que haga la funcion1
+            alert('funcion1');
+        },
+
+        funcion2: function HexaJS() {
+            // lo que querés que haga la funcion2
+            alert('funcion2');
+        },
+
+        asignarFuncion: function() {
+            var valor = aplicacion.lista.value;
+
+            if (valor === 'bin') {
+                aplicacion.campoTexto.onclick = aplicacion.funcion1;
+            } else if (valor === 'hexa') {
+                aplicacion.campoTexto.onclick = aplicacion.funcion2;
+            }
+        },
+
+        asignarEventos: function() {
+            aplicacion.lista.onchange = aplicacion.asignarFuncion;
+            aplicacion.asignarFuncion();
+        }
+    };
+
+    window.onload = function() {
+        aplicacion.campoTexto = document.getElementById('select');
+        aplicacion.asignarEventos();
     }
 }
